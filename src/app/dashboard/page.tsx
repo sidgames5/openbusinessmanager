fb20f2819d0e7780b0a8d1e84a34acb74495db2b
Sid <saytleader@outlook.com>
2024-06-13 18:16:54 -0400

    Add editorconfig and format files

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,12 @@
 export default function Page() {
-  return <div className="container">
-    <div className="header">
-        <h1>OpenBusinessManager</h1>
-    </div>
-    <div className="dashboard">
-        <h2>My Businesses</h2>
-        <div className="businesses"></div>
-    </div>
-    <div className="footer"></div>
-  </div>
+	return <div className="container">
+		<div className="header">
+			<h1>OpenBusinessManager</h1>
+		</div>
+		<div className="dashboard">
+			<h2>My Businesses</h2>
+			<div className="businesses"></div>
+		</div>
+		<div className="footer"></div>
+	</div>
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-  return <h1>In development, come back later</h1>
+	return <h1>In development, come back later</h1>
 }
