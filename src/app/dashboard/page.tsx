e826de4b6a31cd685a267d638f0b2944cd8d7bb3
Sid <saytleader@outlook.com>
2024-06-13 17:53:52 -0400

    First commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return <div className="container">
+    <div className="header">
+        <h1>OpenBusinessManager</h1>
+    </div>
+    <div className="dashboard">
+    </div>
+    <div className="footer"></div>
+  </div>
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <h1>In development, come back later</h1>
+}
